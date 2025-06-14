0b7c83f27cc232fdf43774c5f9e81c888c061bd4
Hasna Farhatul <hasna10222068@sttcipasung.ac.id>
2025-06-15 01:29:01 +0700

    history co
    history order

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ document.getElementById("toggleSidebar").addEventListener("click", function () {
     this.innerHTML = "&gt;";
   }
 });
+
+const tabs = document.querySelectorAll('.nav-link');
+  const panes = document.querySelectorAll('.tab-pane');
+
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('active'));
+      panes.forEach(p => p.style.display = 'none');
+
+      tab.classList.add('active');
+      document.getElementById(tab.dataset.tab).style.display = 'block';
+  });
+});
